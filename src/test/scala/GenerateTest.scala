01623894434f100540c3422ebb0fa3a23ce846fc
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-06-03 23:12:43 +0900

    Add test

---
+++ src/test/scala/GenerateTest.scala
@@ -0,0 +1,24 @@
+import bitwise._
+import org.scalatest.{FlatSpec, Matchers}
+
+class GenerateTest extends FlatSpec with Matchers {
+  "Bit generate" should "be success" in {
+    noException should be thrownBy { 1.toBit() }
+    noException should be thrownBy { 2.toBit() }
+    noException should be thrownBy { 2.toBit(2) }
+    noException should be thrownBy { (-1).toBit(2) }
+    noException should be thrownBy { (-1).toBit() }
+    noException should be thrownBy { (-2).toBit(3) }
+    noException should be thrownBy { (-2).toBit() }
+    noException should be thrownBy { 0.toBit() }
+    noException should be thrownBy { 0.toBit(1) }
+  }
+
+  "Bit generate" should "be failure" in {
+    an[IllegalArgumentException] should be thrownBy { 1.toBit(0) }
+    an[IllegalArgumentException] should be thrownBy { 0.toBit(0) }
+    an[IllegalArgumentException] should be thrownBy { 2.toBit(1) }
+    an[IllegalArgumentException] should be thrownBy { (-1).toBit(1) }
+    an[IllegalArgumentException] should be thrownBy { (-1).toBit(0) }
+  }
+}
