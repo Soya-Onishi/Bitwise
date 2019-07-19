2b460a96b3889fee166cc05b367f4757dc05137d
Soya Ohnishi <ohnishi_soya@s.okayama-u.ac.jp>
2019-07-19 20:09:01 +0900

    Fix signature of type conversion method

--- src/main/scala/bitwise/package.scala
+++ src/main/scala/bitwise/package.scala
@@ -1,12 +1,13 @@
 import scala.language.implicitConversions
 
 package object bitwise {
-  implicit def fromBooleanTransformer(b: Boolean): FromIntegerToBit = {
-    val v = if(b) 1 else 0
-    new FromIntegerToBit(BigInt(v))
+  implicit def fromIntTransformer[T](n: T)(implicit ev: Int with Long with Boolean <:< T): FromIntegerToBit = n match {
+    case bool: Boolean =>
+      val v = if(bool) 1 else 0
+      new FromIntegerToBit(BigInt(v))
+    case num: Int => new FromIntegerToBit(BigInt(num))
+    case num: Long => new FromIntegerToBit(BigInt(num))
   }
-
-  implicit def fromIntTransformer(n: Int): FromIntegerToBit = new FromIntegerToBit(BigInt(n))
-  implicit def fromLongTransformer(n: Long): FromIntegerToBit = new FromIntegerToBit(BigInt(n))
+  // implicit def fromLongTransformer(n: Long): FromIntegerToBit = new FromIntegerToBit(BigInt(n))
   implicit def fromStringTransformer(s: String): FromStringToBit = new FromStringToBit(s)
 }
